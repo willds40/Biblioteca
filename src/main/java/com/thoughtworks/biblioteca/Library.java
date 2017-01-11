8909cc28e87396c0178f63abbe243f00a38a56f7
gmartine <gmartine@thoughtworks.com>
2017-01-11 16:52:05 -0600

    Returning empty list of books

--- src/main/java/com/thoughtworks/biblioteca/Library.java
+++ src/main/java/com/thoughtworks/biblioteca/Library.java
@@ -1,7 +1,19 @@
 package com.thoughtworks.biblioteca;
 
+import java.util.ArrayList;
+
 /**
  * Created by gmartine on 1/11/17.
  */
 public class Library {
+
+    public String start() {
+        return "Welcome";
+    }
+
+
+    public ArrayList<String> printListOfBooks() {
+        ArrayList<String> listOfBooks = new ArrayList();
+        return listOfBooks;
+    }
 }
--- src/test/java/com/thoughtworks/biblioteca/LibraryTest.java
+++ src/test/java/com/thoughtworks/biblioteca/LibraryTest.java
@@ -1,10 +1,32 @@
 package com.thoughtworks.biblioteca;
+import static org.junit.Assert.assertThat;
+
+
+import org.junit.Test;
 
 import static org.junit.Assert.*;
+import org.hamcrest.CoreMatchers.*;
+
+import java.util.ArrayList;
 
 /**
  * Created by gmartine on 1/11/17.
  */
 public class LibraryTest {
+
+    @Test
+    public void shouldReturnWelcome(){
+       Library library = new Library();
+       library.start();
+       assertThat(library.start(), org.hamcrest.CoreMatchers.is("Welcome"));
+    }
+
+    @Test
+    public void shouldReturnList(){
+        Library library = new Library();
+        ArrayList<String> listOfBooks = new ArrayList();
+        assertThat(library.printListOfBooks(), org.hamcrest.CoreMatchers.is(listOfBooks));
+
+    }
 
 }
